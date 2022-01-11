6f1d20c9a78153e1a1fa4d20fc6fd3b18f5f1713
Jashema Panapa <53614792+ShemaxCodes@users.noreply.github.com>
2022-01-11 16:09:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 
+const friends = ["Paris", "Meghan", "Toni"];
+friends.push("Toa");
+console.log(friends);
+
+friends.unshift("John"); // Places John at the beginning of the array
+console.log(friends);
